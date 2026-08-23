b2a539e926808ef3e909ed1d41fefcb4e6a258f6
Alice <wahidi.mouad@student.emi.ac.ma>
2026-08-23 03:56:08 +0100

    Add: longest-substring-without-duplicates - submission-20

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-20.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str 
+        :rtype: int
+        """
+        length = 0
+        new_length = 0
+        max_length = 0
+        D={}
+        for i in range(len(s)):
+            if s[i] not in D.keys():
+               D[s[i]]=i
+               length+=1
+            else:
+                if i-length<=D[s[i]]:
+                    new_length = i-D[s[i]]
+                    length = new_length  
+                else:
+                    length+=1
+                D[s[i]]=i
+            max_length=max(max_length,length)
+        return max_length
+            
+           
+ 
